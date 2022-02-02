39e9bea8604149488ebe0551c0b068b9bdb89979
brookewebb <kuonisacoolguy@hotmail.com>
2022-02-02 07:21:42 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+
+var counter = 9
+function likeThis(){
+    counter++
+    document.querySelector('#like1').innerHTML = counter
+
+}
+
+
+var counter2 = 12
+
+function likeThisTwo(){
+    counter2++
+    document.querySelector('#like2').innerHTML = counter2 
+}
+
+var counter3 = 9
+
+function likeThisThree(){
+    counter3++
+    document.querySelector('#like3').innerHTML = counter3
+}
